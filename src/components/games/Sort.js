b60077833c0f525c90322b2273717b9e17ee7a3d
Brett Abramson <brett.m.abramson@gmail.com>
2023-04-14 09:09:08 -0500

    created and tested search by name function for games

--- src/components/games/Sort.js
+++ src/components/games/Sort.js
@@ -7,13 +7,16 @@ const Sort = (props) => {
     const [gameGenre, setGameGenre] = useState([])
     let gameGenres = []
     const data = [...props.games]
-    // used by the genre search
-    const [filter, setFilter] = useState("")
-
+    // ===  used by the genre search    ===
+    const [genreSearch, setGenreSearch] = useState("")
+    const [nameSearch, setNameSearch] = useState("")
     const genreToggle = () => setGenres(!genres)
     
-    const handleFilterChange = (event) => {
-        setFilter(event.target.value)
+    const handleGenreSearch = (event) => {
+        setGenreSearch(event.target.value)
+    }
+    const handleNameSearch = (event) => {
+        setNameSearch(event.target.value)
     }
     
     // this function currently just reverses the order they are rendered, nothing specific to name...
@@ -23,11 +26,18 @@ const Sort = (props) => {
         // props.setGames(sortedData) :
         props.setGames(sortedData.reverse())
     }
-    const genreFilter = (event) => {
+    const searchByName = (event) => {
         event.preventDefault()
-        // checks to see if genre matches with what is written
+        const filterByName = (item) => {
+            if (item.name.toLowerCase() === nameSearch.toLowerCase()) {return true}
+        }
+        props.setGames(data.filter(filterByName))
+    }
+    const searchGenre = (event) => {
+        event.preventDefault()
+        // checks to see if genre matches with what user writes
         const filterByGenre = (item) => {
-            if (item.game_genre.toLowerCase() === filter.toLowerCase()){return true}      
+            if (item.game_genre.toLowerCase() === genreSearch.toLowerCase()){return true}      
         }
         // runs through the array filtering what doesn't pass our comparison function
         props.setGames(data.filter(filterByGenre))
@@ -41,7 +51,7 @@ const Sort = (props) => {
         <>
             <button onClick={()=>props.getGames()}>Reset</button>
             <button onClick={sortByName}>Name</button>
-            {/* button display to pick genres? */}
+            {/* OR button display to pick genres? */}
             <button onClick={genreToggle}>Genres</button>
             {genres ?
                 <div className="filter-btns">
@@ -52,7 +62,7 @@ const Sort = (props) => {
                 </div>
                 :
                 null}      
-             {/* dropdown menu to display different genres? */}
+             {/* OR dropdown menu to display different genres? */}
             <div className="select-genre">
                 <form onChange={filterBy}>
                     <label htmlFor="genre-select">Filter by Genre</label>
@@ -67,11 +77,17 @@ const Sort = (props) => {
             </div>
                 
 
-                    {/* search to display different genres? --this way currently only utilizes the frontend-- */}
-            <div className="genreFilter-input">
-                <form onSubmit={genreFilter}>
-                <input type="text" onChange={handleFilterChange}></input>
+                    {/* OR search to display different genres? --this way currently only utilizes the frontend-- */}
+            <div className="searchGenre-input">
+                <form onSubmit={searchGenre}>
+                <input type="text" onChange={handleGenreSearch} />
                 <button>Search By Genre</button>
+                </form>
+            </div>
+            <div className="searchName-input">
+                <form onSubmit={searchByName}>
+                    <input type="text" onChange={handleNameSearch} />
+                    <button>Search By Name</button>
                 </form>
             </div>
         </>
